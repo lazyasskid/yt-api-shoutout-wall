b195cca85a76a9899f83deda24228fbe018b2174
lazyasskid <71947718+lazyasskid@users.noreply.github.com>
2020-09-27 05:39:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,17 +4,15 @@ Single key can refresh the wall every 45 seconds, while 9 keys are needed for a 
 of 5 seconds!
 */
 
+// API Key
 var APIKeys = ["AIzaSyDDqp0q5t3a9XUH9aZRx32mVn9Bvg3b14Y"];
-
+// number of channel to display
 var channelcount = 10;
-
-//Default Name
+// default channel name
 var chanName = "Unknown";
-
-//Background Color
+// background color
 var bgColor = "rgba(255, 255, 255,)";
-
-//Default Image url 
+// default image url
 var imgUrl = "https://yt3.ggpht.com/a-/AAuE7mB98CJL1Ye38OXbGM8WMR8lJVJRV_kXU1utHA=s240-mo-c-c0xffffffff-rj-k-no";
 
 $('body').css("background",bgColor);
